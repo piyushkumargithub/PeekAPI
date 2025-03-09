02f3eba59b1b28815209e43c141034a1b88b9dd8
Piyush <piyushk123umar@gmail.com>
2025-03-09 16:57:38 +0530

    added basic requests validation for input

--- src/ui/request_panel.py
+++ src/ui/request_panel.py
@@ -1,4 +1,7 @@
 import customtkinter as ctk
+from CTkMessagebox import CTkMessagebox
+import re
+import json
 
 class RequestPanel(ctk.CTkFrame):
     def __init__(self, parent, on_send_request):
@@ -8,13 +11,13 @@ class RequestPanel(ctk.CTkFrame):
         
         #URL Input
         self.url_entry = ctk.CTkEntry(self, placeholder_text="Enter URL")
-        self.url_entry.pack(pady=10, fill='x')
+        self.url_entry.pack(pady=10,padx=30, fill='x')
 
         #Request Method Selector 
         self.method_var= ctk.StringVar(value="GET")
         self.method_menu = ctk.CTkOptionMenu(self, variable=self.method_var, 
                                              values=["GET", "POST", "PUT", "DELETE", "PATCH"])
-        self.method_menu.pack(pady=10, fill='x')
+        self.method_menu.pack(pady=10,padx=100, fill='x')
 
         #Headers Input
         self.headers_entry = ctk.CTkEntry(self, placeholder_text="Enter Headers")
@@ -30,9 +33,42 @@ class RequestPanel(ctk.CTkFrame):
         self.send_button.pack(pady=20)
 
     def send_request(self):
-        url = self.url_entry.get()
+        url = self.url_entry.get().strip()
         method = self.method_var.get()
-        headers = self.headers_entry.get()
+        headers = self.headers_entry.get().strip()
         body = self.body_entry.get('0.0', 'end').strip()
 
+
+        #validate URL
+        if not self.is_valid_url(url):
+            CTkMessagebox(title="Invalid URL", message="Please enter a valid URL",icon="cancel", option_1="OK")
+            return
+        
+        #validate Headers
+        parsed_headers = None
+        if headers:
+            try:
+                parsed_headers = json.loads(headers)
+                if not isinstance(parsed_headers, dict):
+                    raise ValueError("Headers must be a JSON object")
+            except json.JSONDecodeError:
+                CTkMessagebox(title="Invalid Headers", message="Headers must be a valid JSON object",icon="cancel", option_1="OK")
+                return
+            
+        #validate Body 
+        if method in ["POST", "PUT", "PATCH"] and not body:
+            CTkMessagebox(title="Invalid Body", message="Request body is empty for a write method",icon="warning", option_1="OK")
+        
+        
         self.on_send_request(url, method, headers, body)
+
+    def is_valid_url(self, url):
+        url_pattern=re.compile(
+            r'^(https?://)?'
+            r'((([a-zA-Z0-9\-_]+\.)+[a-zA-Z]{2,})'
+            r'|localhost'
+            r'|\d{1,3}(\.\d{1,3}){3})'
+            r'(:\d+)?'
+            r'(/.*)?$'
+        )
+        return re.match(url_pattern, url)
